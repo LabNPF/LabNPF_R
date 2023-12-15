7d875fa7cf8882f1ce7c3941fef9876890ddcca9
Fernando Ferreira-Santos <ferreira-santos@users.noreply.github.com>
2023-12-15 11:00:47 +0000

    Add files via upload

---
+++ 2023-12-15 Hackaton R Meta-analysis LabNPF/2023-12-15 Hackaton LabNPF_blank.R
@@ -0,0 +1,46 @@
+# 1. Initialize ####
+
+## Package para gestão de packages: pacman
+library(pacman)
+
+## Carregar package de meta-análise "metafor" (auto-instala se for necessário)
+p_load(metafor)
+
+
+## Mostrar versões em uso do R e do package metafor
+??
+??
+
+## Abrir a documentação de ajuda do package metafor
+??
+
+
+
+
+# 2. Import data #### 
+
+## Definir pasta/diretório de trabalho
+??
+
+## Abrir ficheiro da base de dados "P200_hackaton.csv"
+##     Disponível em https://github.com/LabNPF/LabNPF_R/blob/main/2023-12-15%20Hackaton%20R%20Meta-analysis%20LabNPF/P200_hackaton.csv
+??
+
+
+## Visualizar dados
+??
+  
+  
+  
+  # 3. Meta-analysis (and diagnostics) ####
+?? ¯\_(ツ)_/¯
+  
+  
+  
+  # 4. Heterogeneity analysis ####
+?? ¯\_(ツ)_/¯
+  
+  
+  
+  
+  
